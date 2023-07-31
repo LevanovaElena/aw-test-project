3033c9799e1fd9c46e6350d1f7b7935c0fb8c50d
ellevanova <ellevanova@gmail.com>
2023-07-31 13:46:18 +0400

    added fonts and icons

---
+++ src/models.d.ts
@@ -0,0 +1,10 @@
+export declare type TimeLineItem={
+
+    departureTime: string,
+    arrivalTime: string,
+    confirmationNumber: string,
+    depCode: string,
+    arrCode: string,
+    description: string,
+    id: string
+};
---
+++ src/modules/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="">Hello</span>
+      <h1 className="">Hello</h1>
+    </div>
+  );
+}
+
+export default App;

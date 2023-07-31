81df22c3520681389c8327e4f2e09469a865247f
ellevanova <ellevanova@gmail.com>
2023-07-31 19:21:52 +0400

    added buttons

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../../styles/index.scss';
+import {ButtonIcon} from "../common/button-icon.component";
 
 function App() {
-  return (
-    <div className="App">
-      <span className="">Hello</span>
-      <h1 className="">Hello</h1>
-    </div>
-  );
+    return (
+        <div className="app">
+            <span className="">Hello</span>
+            <h1 className="">Hello</h1>
+            <ButtonIcon type={"load"} onClick={() => {console.log("load");}} isActive={false} caption={'Load future itineraries'}/>
+            <ButtonIcon type={"load"} onClick={() => {console.log("load");}} isActive={true} caption={'Load future itineraries'}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/modules/common/button-icon.component.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {JSX} from "react";
+
+import LoadIcon from "../../images/load-icon.svg";
+import LoadIconActive from "../../images/load-icon-active.svg";
+
+export declare type ButtonIconProps = {
+    type: 'load',
+    onClick: () => void,
+    caption: string,
+    isActive?: boolean
+};
+
+export const getIcon = (type: 'load', isActive?: boolean) => {
+    switch (type) {
+        case "load":
+            return isActive ? <img src={LoadIconActive} alt="Load" className='button-icon__img'/> :
+                <img src={LoadIcon} alt="Load" className='button-icon__img'/>;
+    }
+
+}
+export const ButtonIcon = ({caption, type, onClick, isActive}: ButtonIconProps): JSX.Element => {
+    return (
+        <button className={`button-icon ${isActive ? 'button-icon_active' : ''}`} onClick={() => onClick()}>
+            {getIcon(type, isActive)}
+            <div className='button-icon__caption'>{caption}</div>
+        </button>
+    );
+
+}
